581635c0143a2db5a47a04bbcdd92b5990bf5d47
git <mapi@sangut.cl>
2024-02-14 11:44:24 +0100

    fix(domain/get-employees): change return type of the method

--- src/main/kotlin/employeesmanagement/domain/usecase/impl/GetEmployeesUseCaseImpl.kt
+++ src/main/kotlin/employeesmanagement/domain/usecase/impl/GetEmployeesUseCaseImpl.kt
@@ -1,18 +1,15 @@
 package employeesmanagement.domain.usecase.impl
 
-import employeesmanagement.domain.model.DomainError
 import employeesmanagement.domain.model.Employee
 import employeesmanagement.domain.repository.EmployeeRepository
 import employeesmanagement.domain.usecase.GetEmployeesUseCase
 import employeesmanagement.util.Either
 import javax.inject.Inject
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 class GetEmployeesUseCaseImpl @Inject constructor(
     private val repository: EmployeeRepository
 ): GetEmployeesUseCase {
-    override fun invoke(): Either<DomainError, List<Employee>> {
-        return Either.Success(repository.getLocalEmployees())
+    override fun invoke(): List<Employee> {
+        return Either.Success(repository.getLocalEmployees()).success
     }
 }
