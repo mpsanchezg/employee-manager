f33ef656634154db197b5e2bf21cbe6896f94983
git <mapi@sangut.cl>
2024-02-14 13:38:27 +0100

    feat(presentation/app): add user options logic

--- src/main/kotlin/employeesmanagement/presentation/ManagerApplication.kt
+++ src/main/kotlin/employeesmanagement/presentation/ManagerApplication.kt
@@ -8,7 +8,6 @@ import employeesmanagement.data.network.api.ApiConfig
 import employeesmanagement.data.network.api.BaseUrls
 import employeesmanagement.data.network.retrofit.RetrofitClient
 import employeesmanagement.data.repository.impl.EmployeeRepositoryImpl
-import employeesmanagement.domain.model.Employee
 import employeesmanagement.domain.usecase.DeleteEmployeesUseCase
 import employeesmanagement.domain.usecase.GetEmployeesUseCase
 import employeesmanagement.domain.usecase.PopulateEmployeesUseCase
@@ -20,77 +19,121 @@ import employeesmanagement.employeesmanagement.domain.usecase.impl.AddNewEmploye
 import employeesmanagement.presentation.EmployeeManager
 import employeesmanagement.util.orZero
 import employeesmanagement.util.toCurrency
-import java.util.Random
 
 class ManagerApplication {
-    private val localStore = LocalStore()
-    private val local = EmployeeLocalDataSourceImpl(localStore)
+    private val local = EmployeeLocalDataSourceImpl(LocalStore())
     private val remote = EmployeeRemoteDataSourceImpl(
-        RetrofitClient(ApiConfig(
-        baseUrl = BaseUrls.RANDOM_USER_BASE,
-        token = ""
-        ), RandomUserApi::class.java)
+        RetrofitClient(
+            ApiConfig(baseUrl = BaseUrls.RANDOM_USER_BASE, token = ""),
+            RandomUserApi::class.java
+        )
     )
     private val repository = EmployeeRepositoryImpl(remote, local)
-    private val populateEmployeesUseCase: PopulateEmployeesUseCase = PopulateEmployeesUseCaseImpl(repository )
+    private val populateEmployeesUseCase: PopulateEmployeesUseCase = PopulateEmployeesUseCaseImpl(repository)
     private val getEmployeesUseCase: GetEmployeesUseCase = GetEmployeesUseCaseImpl(repository)
     private val deleteEmployeesUseCase: DeleteEmployeesUseCase = DeleteEmployeesUseCaseImpl(repository)
     private val addNewEmployeeUseCase: AddNewEmployeeUseCase = AddNewEmployeeUseCaseImpl(repository)
+    private val employeeManager: EmployeeManager
 
-    var employeeManager: EmployeeManager = EmployeeManager(
-        populateEmployeesUseCase,
-        getEmployeesUseCase,
-        deleteEmployeesUseCase,
-        addNewEmployeeUseCase
-    )
+    init {
+        println("-------------------------------------")
+        println("BIENVENIDO/A AL GESTOR DE EMPLEADOS")
+        println("-------------------------------------\n")
+
+        employeeManager = EmployeeManager(
+            populateEmployeesUseCase,
+            getEmployeesUseCase,
+            deleteEmployeesUseCase,
+            addNewEmployeeUseCase
+        )
+    }
 
     fun start(): Boolean {
-        println("Ingrese la acción que quiera realizar:\n")
-        println("1. Agregar un nuevo empleado\n")
-        println("2. Ver el detalle de un empleado\n")
-        println("3. Eliminar un empleado\n")
-        println("4. Mostrar lista de empleados")
+        printActionOptions()
+
         val action = readlnOrNull()
+        return handleUserOption(action)
+    }
 
-        if (action != null) {
-            when (action) {
-                "1" -> {
-                    addNewEmployeeAction()
-                }
-                "2" -> {
+    fun finish() {
+        println("¡Vuelve pronto!")
+    }
 
-                }
-                "3" -> {}
-                "4" -> {}
-                else -> {
-                    return@start false
-                }
-            }
+    private fun printActionOptions() {
+        println("Ingrese el número de la acción que quiera realizar:")
+        println("1. Agregar un nuevo empleado")
+        println("2. Ver el detalle de un empleado")
+        println("3. Eliminar un empleado")
+        println("4. Mostrar lista de empleados")
+        println("Presione cualquier otra tecla para salir.")
+    }
+
+    private fun handleUserOption(action: String?): Boolean {
+        when (val actionType: ActionType? = ActionType.values().find { it.ordinal.toString() == action }) {
+            ActionType.ADD_EMPLOYEE -> printActionWelcome(actionType, ::addNewEmployeeAction)
+            ActionType.SHOW_EMPLOYEE_DETAILS -> printActionWelcome(actionType, ::showEmployeeDetailsAction)
+            ActionType.REMOVE_EMPLOYEE -> printActionWelcome(actionType, ::removeEmployeeAction)
+            ActionType.SHOW_ALL_EMPLOYEES -> printActionWelcome(actionType, ::showEmployeesListAction)
+            ActionType.FINISH -> return false
+            else -> return false
         }
         return true
     }
 
-    private fun addNewEmployeeAction() {
-        println("---- NUEVO USUARIO ---")
-        println("Si no desea continuar, ingrese \"c\" o \"C\"")
-        val userContinue = readlnOrNull()
-        if (userContinue != null && userContinue.lowercase() == "c") return
+    private fun printActionWelcome(actionType: ActionType, action: () -> Unit) {
+        println("--------------- ${actionType.title} --------------------")
+        if (actionType != ActionType.SHOW_ALL_EMPLOYEES) {
+            println("Presione Enter para continuar, para volver al menú ingrese cualquier otro comando.")
+            val userContinue = readlnOrNull()
+            if (!userContinue.isNullOrBlank()) return
+        }
+        action()
+        println("-------------------------------------------------------")
+    }
 
+    private fun addNewEmployeeAction() {
         println("Ingrese el nombre: ")
-        val name = readlnOrNull()
+        val name = readlnOrNull().orEmpty()
         println("Ingrese la edad: ")
         val age = readlnOrNull()?.toInt().orZero()
         println("Ingrese el salario: ")
         val salary = readlnOrNull()?.toInt().orZero().toCurrency()
         println("Ingrese el email: ")
-        val email = readlnOrNull()?.toInt().orZero().toCurrency()
+        val email = readlnOrNull().orEmpty()
 
-        employeeManager.addNewEmployee(Employee(
-            id = name.orEmpty(),
-            name.orEmpty(),
-            age,
-            salary,
-            email
-        ))
+        employeeManager.addNewEmployee(name, age, salary, email)
+        printEmployeesList("Lista de empleados actualizada")
+    }
+
+    private fun showEmployeeDetailsAction() {
+        println("Ingrese el nombre del empleado:")
+        val name = readlnOrNull()
+
+        employeeManager.showEmployeeByName(name.orEmpty())
+    }
+
+    private fun removeEmployeeAction() {
+        printEmployeesList("Lista de empleados")
+
+        println("Ingrese el nombre del empleado que desea eliminar, presione Enter para volver al menú: ")
+        val name = readlnOrNull()
+        if (name.isNullOrBlank()) return
+
+        employeeManager.removeEmployees(name)
+    }
+
+    private fun showEmployeesListAction() = employeeManager.showEmployeesList()
+
+    private fun printEmployeesList(message: String) {
+        println("$message:")
+        employeeManager.showEmployeesList()
+    }
+
+    enum class ActionType(val title: String? = null) {
+        FINISH,
+        ADD_EMPLOYEE("NUEVO EMPLEADO"),
+        SHOW_EMPLOYEE_DETAILS("DETALLES EMPLEADO"),
+        REMOVE_EMPLOYEE("ELIMINAR EMPLEADO"),
+        SHOW_ALL_EMPLOYEES("LISTA DE EMPLEADOS")
     }
 }
